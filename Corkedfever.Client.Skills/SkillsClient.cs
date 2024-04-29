62552e1ff8a50c734c7e8191608f4d5629c1ae80
Jasmine <revan250@yahoo.com>
2024-04-29 13:46:46 -0400

    Added Clients and Test section

---
+++ Corkedfever.Client.Skills/SkillsClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Corkedfever.Client.Skills
+{
+    public class SkillsClient : SkillsClientBase
+    {
+        public SkillsClient(string baseUrl) : base(baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+    }
+}
